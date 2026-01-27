2c74e8fa23daac168f6997bb1758906f5de294f7
Francisco Zanartu <fzanartu@outlook.com>
2026-01-27 12:23:58 -0300

    feat: enhance llm instantiation with structured imports and additional Google LLM configuration

--- src/llm/llms.py
+++ src/llm/llms.py
@@ -1,12 +1,27 @@
 "use langchain to instantiate llms"
 
+import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from google.genai import types
 
 load_dotenv()
 
 google_llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash", temperature=0, max_tokens=128000
+    model="gemini-2.5-flash",
+    temperature=0,
+    max_tokens=128000,
 )
-openai_llm = ChatOpenAI(model="gpt-5-mini-2025-08-07", temperature=0, max_tokens=128000)
+openai_llm = ChatOpenAI(model="gpt-5-mini-2025-08-07", temperature=0)
+
+google_search_tool = types.Tool(google_search=types.GoogleSearch())
+google_llm_pro = ChatGoogleGenerativeAI(
+    model="gemini-2.5-pro",
+    temperature=0.0,
+    max_tokens=128000,
+)
+
+google_llm_with_search_tool = google_llm.bind_tools(
+    tools=[google_search_tool],
+)
